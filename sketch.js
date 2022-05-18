8621d5a779a791cc73ac9013ed82db8e722aa28c
LAVALEUWU <91077923+LAVALEUWU@users.noreply.github.com>
2022-05-18 17:50:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var bg,h,inpng
+
+function preload() {
+  bg = loadImage("bg.png");
+  h = loadImage("h.png");
+  inpng = loadImage("in.png");
+
+function setup() {
+  createCanvas(1000,500);
+  //?
+}
+
+function draw() {
+  background(bg);
+ 
+ 
+
+  drawSprites()
+}
